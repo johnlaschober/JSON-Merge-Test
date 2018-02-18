20ed456de58189c1beaca417563dd72121ebe8da
johnlaschober <johnlaschober@gmail.com>
2018-02-18 15:00:11 -0600

    Test

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 var http = require('http');
 
-var server = http.createServer(function(request, response) {
+<script type="text/javascript" scr="https://johnlaschobersoftwareengineering.azurewebsites.net/info.json"></script>
+
+
+
+var server = http.createServer(function(request, response) 
+{
 
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello World! This is John's nodeJS website!");
