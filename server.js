917ae78997b94fc12bb43f12655e4fc873b4172b
johnlaschober <johnlaschober@gmail.com>
2018-02-18 16:04:35 -0600

    Calling JSON objects

--- server.js
+++ server.js
@@ -1,14 +1,43 @@
 var http = require('http');
 
-<script type="text/javascript" scr="https://johnlaschobersoftwareengineering.azurewebsites.net/info.json"></script>
+var allURLS = ["https://johnlaschobersoftwareengineering.azurewebsites.net/info.json", "http://alisp18.azurewebsites.net/myinfo.json"];
+var allJSON = [];
 
+var request = require('request'); // Imports library
 
+for (i = 0; i < allURLS.length; i++)
+{
+	request(allURLS[i], function (error, response, body)
+	{
+		if (!error && response.statusCode == 200) 
+		{
+			var importedJSON = JSON.parse(body);
+			console.log(importedJSON);
+			allJSON[i] = JSON.stringify(importedJSON);
+			console.log(allJSON[i]);
+		}
+	});
+}
+
+// Current problems:
+// 1) Storing JSON as String in array simply to try to display it on the site
+//  	^ Not recommended for final project, just for testing
+// 2) I am awful at Javascript
+// 3) allJSON array can successfully output to console at lines 15 and 17
+//		but won't ouput when the server is called (array returns undefined).
+//		Likely a javascript thing I don't know about.
+
+// I think this is a good start to pull the JSON objects at least.
 
 var server = http.createServer(function(request, response) 
 {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World! This is John's nodeJS website!");
+	for (i = 0; i < allURLS.length; i++)
+	{
+		//response.end(allJSON[i]); // Formatted as a string without linebreaks yuck
+		console.log(allJSON[i]);
+	}
 
 });
 
