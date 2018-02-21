0fafed60a705c810ba39bbe933e49cb687d01c64
johnlaschober <johnlaschober@gmail.com>
2018-02-20 18:39:48 -0600

    Clarification on JSON storage

--- server.js
+++ server.js
@@ -1,32 +1,38 @@
+var request = require('request'); // Imports libraries
 var http = require('http');
 var fs = require('fs');
 
+// Define URLs here
 var allURLS = ["https://johnlaschobersoftwareengineering.azurewebsites.net/info.json", "http://alisp18.azurewebsites.net/myinfo.json"];
+// Where JSON objects will be stored
 var allJSON = [];
 
-var request = require('request'); // Imports library
-var jsonData;
-for (i = 0; i < allURLS.length; i++)
+
+
+
+
+for (i = 0; i < allURLS.length; i++) // Initiates on server launch
 {
 	request(allURLS[i], function (error, response, body)
 	{
 		if (!error && response.statusCode == 200) 
 		{
 			var importedJSON = JSON.parse(body);
-			
-			//console.log(importedJSON);
 			allJSON.push(importedJSON);
 			console.log(allJSON.length);
 			if (allJSON.length == allURLS.length)
 			{
 				// You have all JSON objects
 				// Now do stuff
+				// Like make a big, concatenated JSON file
+				// idk
 			}
 		}
 	});
-}
+} // Should make this a function so we call updates whenever
 
-fs.writeFile("test.txt", jsonData, function(err) 
+var jsonData; // Should be set to the final concatenated megaJSON file eventually
+fs.writeFile("test.txt", jsonData, function(err)  // File writer for saving a json file, not done
 {
     if(err) 
 	{
@@ -34,14 +40,14 @@ fs.writeFile("test.txt", jsonData, function(err)
     }
 });
 
-var server = http.createServer(function(request, response) 
+var server = http.createServer(function(request, response)  // On user connect
 {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
 
 	for (j = 0; j < allURLS.length; j++)
 	{
-		console.log(allJSON[j]);
+		console.log(allJSON[j]); // Working correctly, verifies we have our data stored correctly
 	}
 
 });
