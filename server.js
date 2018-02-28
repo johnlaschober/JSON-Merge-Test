c27be5a0b59734cf35d384914393f0e28037d139
johnlaschober <31740538+johnlaschober@users.noreply.github.com>
2018-02-27 19:32:02 -0600

    Major rewrite
    Information stored locally when server is started. Defined as a function to call it easier.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-var request = require('request'); // Imports libraries
 var http = require('http');
+
+var request = require('request'); // Imports libraries
 var fs = require('fs');
 
 // Define URLs here
@@ -8,65 +9,73 @@ var allURLS = ["https://csoftware.azurewebsites.net/groupinfo.json", "https://jo
 var allJSON = [];
 
 var concattedJSON;
-var finalJSON;
-for (i = 0; i < allURLS.length; i++) // Initiates on server launch
+var finalJSON = "no";
+var done = false;
+
+var indexStack = [];
+
+for (i = 0; i < allURLS.length; i++)
 {
-	request(allURLS[i], function (error, response, body)
+	indexStack.push(i);
+	request(allURLS[i], function (error, response, body) 
 	{
+		console.log(response.statusCode);
 		if (!error && response.statusCode == 200) 
 		{
+			var iterator = indexStack[indexStack.length - 1];
+			indexStack.pop();
 			var importedJSON = JSON.parse(body);
 			allJSON.push(importedJSON);
-			//console.log(allJSON.length);
-			if (allJSON.length == allURLS.length)
+			var outputString = JSON.stringify(importedJSON, null, 4);
+			fs.writeFile("member" + iterator + ".json", outputString, 'utf-8', function (err)  // File writer for saving a json file, not done
 			{
-				// You have all JSON objects
-				// Now do stuff
-				// Like make a big, concatenated JSON file
-				// idk
-				var concattedJSON = "";
-				concattedJSON = '{"members":[';
-				for (j = 0; j < allURLS.length; j++)
+				if (err) 
 				{
-					concattedJSON += JSON.stringify(allJSON[j]);
-					if (j != allURLS.length - 1)
+					return console.log(err);
+				}
+				else
+				{
+					if (indexStack.length == 0)
 					{
-						concattedJSON += ',';
+						var concattedJSON = "";
+						concattedJSON = '{"members":[';
+						for (j = 0; j < allJSON.length; j++)
+						{
+							concattedJSON += JSON.stringify(allJSON[j]);
+							if (j != allURLS.length - 1) 
+							{
+								concattedJSON += ',';
+							}
+						}
+						concattedJSON += ']}';
+						fs.writeFile("groupJSON.json", concattedJSON, 'utf-8', function (err)  // File writer for saving a json file, not done
+						{
+							if (err) 
+							{
+								return console.log(err);
+							}
+						});
 					}
 				}
-				concattedJSON += ']}';
-				//console.log(concattedJSON);
-				finalJSON = JSON.parse(concattedJSON);
-				console.log(finalJSON);
-				/*
-				var stringFinal = JSON.stringify(finalJSON, null, 4);
-				fs.writeFile("groupJSON.json", stringFinal,'utf-8', function(err)  // File writer for saving a json file, not done
-				{
-					if(err) 
-					{
-						return console.log(err);
-					}
-				});
-				*/
-				
-			}
+			});
+
 		}
 	});
-} // Should make this a function so we call updates whenever
+}
 
-
-
-var server = http.createServer(function(request, response)  // On user connect
+var server = http.createServer(function (request, response)  // On user connect
 {
-
-    response.writeHead(200, {"Content-Type": "text/plain"});
-
-	//var importedJSON = JSON.parse(fs.readFileSync('groupJSON.json', 'utf8'));
-	var displayJSON = JSON.stringify(finalJSON);
-	response.write(displayJSON);
-	response.end();
-	//response.end(importedJSON);
-
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    try
+    {
+        var importedJSON = JSON.parse(fs.readFileSync('groupJSON.json', 'utf8'));    // Reading from input
+        response.write(JSON.stringify(importedJSON, null, 4));
+    }
+    catch (err)
+    {
+        response.write("Something went wrong... " + err);
+    }
+	response.end("");
 });
 
 var port = process.env.PORT || 80;
